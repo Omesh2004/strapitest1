614da5f75b223376b85330178034d04aeadf89d6
Omesh Mehta <omeshmehta70@gmail.com>
2025-05-26 23:13:13 +0530

    middleware cors added

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://your-frontend-domain.com', 'http://localhost:3000'], // Replace with your deployed frontend domain
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
