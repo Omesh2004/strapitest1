439e8cf74b8fa55619f2a1e8bcf851c7be6db2a3
Omesh Mehta <omeshmehta70@gmail.com>
2025-05-27 01:38:54 +0530

    deploye this

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://insti-website.netlify.app/', 'http://localhost:3000'], // Replace with your deployed frontend domain
+      origin: ['https://insti-website.netlify.app', 'http://localhost:3000'], // Replace with your deployed frontend domain
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       headers: '*',
       credentials: true,
